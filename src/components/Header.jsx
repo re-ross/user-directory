8b555ebbeddb1ad4159a1caf9ec34893b26bb6df
Ryan Ross <ryan@reross.com>
2022-03-16 09:42:22 -0500

    started project, added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="header-text"> Home </h1>
+    </header>
+  );
+}
+
+export default Header;
